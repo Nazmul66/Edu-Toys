6f4b1937f299bb99131c1475c00d4eb461a4e28a
Nazmul Hassan <hnazmul748@gmail.com>
2023-05-19 00:55:28 +0600

    error page complete and responsive too

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './ErrorPage.css';
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
+import { FaSadCry } from 'react-icons/fa';
 
 const ErrorPage = () => {
-    const {error} = useRouteError;
-    console.log(error)
+    // .message data, status, error
+    const pageError = useRouteError();
+
     return (
         <div className='error-page'>
-            <div>
-                <h2>404</h2>
+            <div className='max-w-[800px] mx-auto'>
+                <div className='flex justify-center'>
+                   <FaSadCry className="text-[yellow] text-[200px]" />
+                </div>
+                <h2 className="text-[yellow] lg:text-[100px] text-[80px] font-extrabold text-center"> {pageError?.status || 404}</h2>
+                <p className="text-[yellow] lg:text-[40px] text-[24px] font-extrabold text-center">{pageError?.data}</p>
+                <div className='text-center mt-8'>
+                    <Link to="/" className='py-4 px-12 rounded-[4px] font-bold bg-cyan-800 text-white'>Go to Home</Link>
+                </div>
             </div>
         </div>
     );
